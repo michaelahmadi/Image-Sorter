a281919dda6bc3530c8ab187436c178ea63b1dde
Michael Ahmadi <mahmad37@uic.edu>
2021-01-16 19:42:55 -0600

    Input handling update

--- ImageSorter.cpp
+++ ImageSorter.cpp
@@ -1,35 +1,26 @@
 #include "ImageSorter.hpp"
 
+// https://www.exiv2.org/examples.html#example1
+// First, complete -> Folder directory + date, check metadata, create new folder with folders based on Dates
 
 int main(int argc, char **argv)
 {
-    
-    if (argc != 2) {
+
+    if (argc != 2)
+    {
         std::cout << "Usage: " << argv[0] << " file\n";
         return 1;
     }
-    
+
     boost::filesystem::path p(argv[1]);
-    
-    if(exists(p))
+
+    if (!is_directory(p))
     {
-
-        if(is_regular_file(p))
-        {
-            std::cout << p << " size is " << file_size(p) << std::endl;
-        }
-        else if(is_directory(p))
-        {
-            std::cout << p << " is a directory" << std::endl;
-        }
-        else
-            std::cout << p << " is neither file nor directory" << std::endl;
-        
+        std::cout << p << " is not a directory" << std::endl;
+        return 1;
     }
-    else
-        std::cout << p << " doesnt exist" << std::endl;
+
 
     
-    std::cout << "Hello world!" << std::endl;
     return 0;
 }
